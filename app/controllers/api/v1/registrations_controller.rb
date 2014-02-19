dc38b69a218650072417c3164b44f2f00a55edea
Ryan Kass <kassr30@gmail.com>
2014-02-19 16:57:15 -0500

    x

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::RegistrationsController < ApplicationController
   
   def create
     user = User.new(:username => params[:username], :password => params[:password], 
-      :phone_number => phoneStrip(params[:phoneNumber]))
+      :phone_number => phoneStrip(params[:phoneNumber]), :auth_token => Digest::SHA1.hexdigest(params[:username] + params[:password]))
     if user.save
-      render :json=> {:auth_token=>Digest::SHA1.hexdigest(user.encrypted_password + user.username), :request=>"sign_up"}, :status=>201
+      render :json=> {:auth_token=> user.auth_token, :request=>"sign_up"}, :status=>201
       return
     else
 #      warden.custom_failure!
