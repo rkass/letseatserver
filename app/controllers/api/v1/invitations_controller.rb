cd690a2dd926c32409252d4bea4fe105b5f09178
Ryan Kass <kassr30@gmail.com>
2014-03-03 11:54:08 -0500

    x

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -1,8 +1,76 @@
+require 'date'
 class ::Api::V1::InvitationsController < ApplicationController
   
+  def makeDateTime(dateString)
+    split = dateString.split(',')
+    monthString = ""
+    monthNum = 0 
+    if split[1].include? "Jan"
+      monthString = "Jan"
+      monthNum = 1 
+    elsif split[1].include? "Feb"
+      monthString = "Feb"
+      monthNum = 2 
+    elsif split[1].include? "Mar"
+      monthString = "Mar"
+      monthNum = 3 
+    elsif split[1].include? "Apr"
+      monthString = "Apr"
+      monthNum = 4 
+    elsif split[1].include? "May"
+      monthString = "May"
+      monthNum = 5 
+    elsif split[1].include? "Jun"
+      monthString = "Jun"
+      monthNum = 6 
+    elsif split[1].include? "Jul"
+      monthString = "Jul"
+      monthNum = 7 
+    elsif split[1].include? "Aug"
+      monthString = "Aug"
+      monthNum = 8 
+    elsif split[1].include? "Sep"
+      monthString = "Sep"
+      monthNum = 9 
+    elsif split[1].include? "Oct"
+      monthString = "Oct"
+      monthNum = 10
+    elsif split[1].include? "Nov"
+      monthString = "Nov"
+      monthNum = 11
+    elsif split[1].include? "Dec"
+      monthString = "Dec"
+       monthNum = 12
+    end 
+    dayOfMonth = (split[1].gsub! monthString, '').to_i
+    splitTime = split[2].split(':')
+    hour = splitTime[0].to_i
+    ampm = "PM"
+    ampm = "AM" unless splitTime[1].include?"PM"
+    hour += 12 if ampm == "PM"
+    minutes = (splitTime[1].gsub! ampm, '').to_i
+    year = Date.today.year
+    year += 1 if Date.today.month > monthNum
+    DateTime.new(year, monthNum, dayOfMonth, hour, minutes)
+  end 
+
   def create
     users = []
-    render :json => {:success => true}, :status=>201
+    users.append(User.find_by_auth_token(params[:auth_token]))
+    if params[:numbers] != nil
+      for number in params[:numbers]
+        for u in User.find_all_by_phone_number(number)
+          users.append(u)
+        end
+      end
+    end
+    p = Preferences.new(params[:foodList], params[:location], params[:price])
+    invitation = Invitation.customNew(users, p, makeDateTime(params[:date]), params[:message])
+    if invitation.save
+      render :json => {:success => true}, :status=>201
+    else
+      render :json => {:success => false}, :status =>422
+    end
     return
   end
 
