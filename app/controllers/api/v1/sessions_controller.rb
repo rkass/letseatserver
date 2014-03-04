fd5d1e32a5db971cde9e6a0b2e14efa3727b660e
Ryan Kass <kassr30@gmail.com>
2014-03-04 09:59:23 -0500

    x

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::SessionsController < ApplicationController
  
     if resource.valid_password?(params[:password])
       sign_in("user", resource)
-      render :json=> {:auth_token=>resource.auth_token, :phone_number => resource.phoneNumber, :request=>"login"}
+      render :json=> {:auth_token=>resource.auth_token, :phone_number => resource.phone_number, :request=>"login"}
       return
     end
     invalid_login_attempt
