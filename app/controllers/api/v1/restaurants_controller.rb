ece0f5d6455bfad4dc2578810e7aa209569d179f
Ryan Kass <kassr30@gmail.com>
2014-03-25 18:39:51 -0400

    x

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -5,28 +5,40 @@ class ::Api::V1::RestaurantsController < ApplicationController
     i = Invitation.find(params[:invitation])
     r = Restaurant.new(params[:name], params[:price], params[:address], params[:types], params[:url], params[:ratingImg], params[:snippetImg])
     i.votes = {} if i.votes == nil
+    retVotes = []
+    skip = true
     for restaurant, vot in i.votes
       if restaurant.url == r.url
-        vot.append(user.id)
+        vot.append(user.id) if (not vot.include?user.id)
         i.save
-        return
+        skip = false
+        retVotes = vot
+        break
       end
     end
-    i.votes[r] = [user.id]
-    i.save
+    if skip
+      i.votes[r] = [user.id]
+      i.save
+      retVotes = [user.id]
+    end
+    render :json => {:success => true, :restaurant => {:user_voted => true, :votes => retVotes.length}, :status => 201 
+    return
   end
 
   def unvote
     user = User.find_by_auth_token(params[:auth_token])
     i = Invitation.find(params[:invitation])
     r = Restaurant.new(params[:name], params[:price], params[:address], params[:types], params[:url], params[:ratingImg], params[:snippetImg])
+    retVotes = []
     for restaurant, vot in i.votes
       if restaurant.url == r.url
         vot.delete(user.id) 
         i.save
-        return
+        retVotes = vot
       end   
     end
+    render :json => {:success => true, :restaurant => {:user_voted => true, :votes => retVotes.length}}, :status => 201 
+    return
   end 
 
 end
