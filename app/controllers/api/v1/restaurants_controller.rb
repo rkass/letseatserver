9e97935876fde57299304e1eaf67d71eb5779163
Ryan Kass <kassr30@gmail.com>
2014-03-25 18:12:10 -0400

    x

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -18,7 +18,8 @@ class ::Api::V1::RestaurantsController < ApplicationController
   def unvote
     user = User.find_by_auth_token(params[:auth_token])
     i = Invitation.find(params[:invitation])
-    r = Restaurant.new(params[:name], params[:price], params[:address], params[:types], params[:url], params[:ratingImg], params[:snippetImg])    for restaurant, vot in i.votes
+    r = Restaurant.new(params[:name], params[:price], params[:address], params[:types], params[:url], params[:ratingImg], params[:snippetImg])
+    for restaurant, vot in i.votes
       if restaurant.url == r.url
         vot.delete(user.id) 
         i.save
