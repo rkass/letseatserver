418a4fe2f9c2a020aea728020c5ba8a0a4f8259d
Ryan Kass <kassr30@gmail.com>
2014-03-25 09:43:19 -0400

    x

--- app/restaurant_finder/places.rb
+++ app/restaurant_finder/places.rb
@@ -15,8 +15,6 @@ class GooglePlaces
     query = CGI::escape(name + " near " + location)
     str = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{query}&sensor=false&key=#{@@api_key}"
     response = JSON.parse(open(str).read)
-    print "Response for #{name}"
-    print response
     for biz in response['results']
      return biz['reference'] if biz['name'] == name
     end
