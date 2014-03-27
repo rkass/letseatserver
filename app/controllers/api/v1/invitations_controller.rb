d5a83a03f3ae3b9a0b4d284b52a13c2586a80e2c
Ryan Kass <kassr30@gmail.com>
2014-03-27 12:33:37 -0400

    x

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -152,9 +152,9 @@ class ::Api::V1::InvitationsController < ApplicationController
   end
 
   def getMeals
-    getInvitationOrMeals(true)
+    getInvitationsOrMeals(true)
   end
   def get
-    getInvitationOrMeals(false)
+    getInvitationsOrMeals(false)
   end    
 end
