c1f8480f9290b359d6326402f01c29017b38c41c
Ryan Kass <kassr30@gmail.com>
2014-04-02 23:28:20 -0400

    x

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -144,6 +144,12 @@ class ::Api::V1::InvitationsController < ApplicationController
         invitation.scheduled = true
         invitation.save
       end
+      if (invitation.id == 132)
+        puts "scheduled"
+        puts invitation.scheduled
+        puts "count"
+        invitation.responses.count - invitation.responses.count(nil)
+      end
     end
   end
 
