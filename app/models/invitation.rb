4e39dae300e6ae032d72863034c067bac5dc2494
Ryan Kass <kassr30@gmail.com>
2014-04-14 13:48:44 -0700

    allowing undecideds

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -28,7 +28,7 @@ class Invitation < ActiveRecord::Base
     (self.creator_index == self.users.index(arguser)) or (responded(arguser) and responses[self.users.index(arguser)].going)
   end
   def declined(arguser)
-    (responded(arguser) and (not responses[self.user.index(arguser)].going))
+    (responded(arguser) and (not responses[self.users.index(arguser)].going))
   end
   def responded(arguser)
     self.responses[self.users.index(arguser)] != nil
