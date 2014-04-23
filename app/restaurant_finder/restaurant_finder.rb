6810e2bfeeb8ab58c20225fd98dff28b2f80a80b
Ryan Kass <kassr30@gmail.com>
2014-04-23 14:38:00 -0400

    x

--- app/restaurant_finder/restaurant_finder.rb
+++ app/restaurant_finder/restaurant_finder.rb
@@ -14,7 +14,7 @@ class RestaurantFinder
       ActiveRecord::Base.connection.reconnect!
       if (not invitation.restaurants.where(url:yelpResult['mobile_url']).exists?)
         isOpenAndPrice = GooglePlaces.isOpenAndPrice(getFormattedAddressFromYelpResult(yelpResult), invitation.dayOfWeek, invitation.timeOfDay)       
-        restaurant = invitation.restaurants.create(:name => yelpResult['name'], :price => isOpenAndPrice.price, :address => yelpResult['location']['display_address'], :url => yelpResult['mobile_url'], :rating_img => yelpResult['rating_img_url'], :snippet_img => yelpResult['image_url'], :rating => yelpResult['rating'], :categories => yelpResult['categories'], :review_count => yelpResult['review_count'], :open_start => isOpenAndPrice.openStart, :open_end => isOpenAndPrice.openEnd, :open => isOpenAndPrice.open, :distance => yelpResult['distance'])
+        restaurant = invitation.restaurants.create({:name => yelpResult['name'], :price => isOpenAndPrice.price, :address => yelpResult['location']['display_address'], :url => yelpResult['mobile_url'], :rating_img => yelpResult['rating_img_url'], :snippet_img => yelpResult['image_url'], :rating => yelpResult['rating'], :categories => yelpResult['categories'], :review_count => yelpResult['review_count'], :open_start => isOpenAndPrice.openStart, :open_end => isOpenAndPrice.openEnd, :open => isOpenAndPrice.open, :distance => yelpResult['distance'])}
         categoryMutex.synchronize{viableOptions += 1} if restaurant.open
       end 
     end
