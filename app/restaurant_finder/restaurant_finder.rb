43ade82c533df2eff860651b42275cc7bd9ca8d1
Ryan Kass <kassr30@gmail.com>
2014-04-24 14:16:05 -0400

    x

--- app/restaurant_finder/restaurant_finder.rb
+++ app/restaurant_finder/restaurant_finder.rb
@@ -105,18 +105,18 @@ class RestaurantFinder
   def searchCategory(viableOptions, category, radius, location, dow, tod,parallel = true)
     assoc_categories = RestaurantFinder.getAssociatedCategories(category)
     viableOptions = viableOptions
-    categoryMutex = Mutex.new
     yelpResults = Yelp.getResults(location, assoc_categories, radius)
     if parallel
       results = Parallel.map(yelpResults) do |yelpResult|
-   #     ActiveRecord::Base.connection.reconnect!
         if (not exists(yelpResult))
           isOpenAndPrice = GooglePlaces.isOpenAndPrice(RestaurantFinder.getFormattedAddressFromYelpResult(yelpResult), dow, tod)
           restDict = {:name => yelpResult['name'], :price => isOpenAndPrice.price, :address => yelpResult['location']['display_address'] * ",", :url => yelpResult['mobile_url'], :rating_img => yelpResult['rating_img_url'], :snippet_img => yelpResult['image_url'], :rating => yelpResult['rating'], :categories => yelpResult['categories'], :review_count => yelpResult['review_count'], :open_start => isOpenAndPrice.openStart, :open_end => isOpenAndPrice.openEnd, :open => isOpenAndPrice.open, :distance => yelpResult['distance']}
-          #categoryMutex.synchronize{viableOptions += 1} if restDict[:open]
-        end 
-#end
+        end
       end
+      results.each do |r|
+        @invitation.restaurants.create(r) if r != nil
+        viableOptions += 1 if r[:open]
+      end 
     else
       yelpResults.each do |yelpResult|
         if (not @invitation.restaurants.where(url:yelpResult['mobile_url']).exists?)
