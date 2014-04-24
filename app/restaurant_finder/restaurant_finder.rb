6e65374eaa49af5445c975f8f563955d1ad73348
Ryan Kass <kassr30@gmail.com>
2014-04-23 20:41:19 -0400

    x

--- app/restaurant_finder/restaurant_finder.rb
+++ app/restaurant_finder/restaurant_finder.rb
@@ -5,7 +5,44 @@ class RestaurantFinder
     @invitation = invitation
     @restaurants = restaurants
     @restaurants_mutex = Mutex.new
-  end  
+  end 
+
+  def self.getAssociatedCategories(category)
+    if category.downcase == "american"
+      return "newamerican,tradamerican,bbq,burgers,cheesesteaks,chicken_wings,comfortfood,sandwiches,soulfood,southern"
+    elsif category.downcase == "cafe"
+      return "bubbletea,coffee,cafes"
+    elsif category.downcase == "chinese"
+      return "chinese"
+    elsif category.downcase == "dessert"
+      return "bakeries,cupcakes,desserts,donuts,gelato,icecream,juicebars,shavedice"
+    elsif category.downcase == "diner"
+      return "diners"
+    elsif category.downcase == "indian"
+      return "indpak"
+    elsif category.downcase == "italian"
+      return "italian,pizza"
+    elsif category.downcase == "japanese"
+      return "japanese,sushi"
+    elsif category.downcase == "korean"
+      return "korean"
+    elsif category.downcase == "mediterranean"  
+      return "mediterranean,greek,mideastern"
+    elsif category.downcase == "mexican"
+      return "mexican,tex-mex,latin"
+    elsif category.downcase == "seafood"
+      return "seafood"
+    elsif category.downcase == "spanish"
+      return "spanish,tapasmallplates"
+    elsif category.downcase == "steakhouse"
+      return "steak"
+    elsif category.downcase == "thai"
+      return "thai"
+    elsif category.downcase == "vegetarian"
+      return "vegetarian,vegan"
+    elsif category.downcase == "vietnamese"
+      return "vietnamese"
+  end
 
   def find(categories, parallel = true)
     if parallel
@@ -15,7 +52,7 @@ class RestaurantFinder
       end
     else
       categories.each do |category|
-        searchCategory(0, category, invitation, 2000, false)
+        searchCategory(0, category, 2000, false)
       end
     end
   end
@@ -30,6 +67,7 @@ class RestaurantFinder
   end
 
   def searchCategory(viableOptions, category, radius, parallel = true)
+    category = RestaurantFinder.getAssociatedCategories
     viableOptions = viableOptions
     categoryMutex = Mutex.new
     yelpResults = Yelp.getResults(invitation.location, category, radius)
@@ -47,9 +85,9 @@ class RestaurantFinder
       end
     else
       yelpResults.each do |yelpResult|
-        if (not invitation.restaurants.where(url:yelpResult['mobile_url']).exists?)
-          isOpenAndPrice = GooglePlaces.isOpenAndPrice(getFormattedAddressFromYelpResult(yelpResult), invitation.dayOfWeek, invitation.timeOfDay)
-          restaurant = invitation.restaurants.create({:name => yelpResult['name'], :price => isOpenAndPrice.price, :address => yelpResult['location']['display_address'] * ",", :url => yelpResult['mobile_url'], :rating_img => yelpResult['rating_img_url'], :snippet_img => yelpResult['image_url'], :rating => yelpResult['rating'], :categories => yelpResult['categories'], :review_count => yelpResult['review_count'], :open_start => isOpenAndPrice.open_start, :open_end => isOpenAndPrice.open_end, :open => isOpenAndPrice.open, :distance => yelpResult['distance']})
+        if (not @invitation.restaurants.where(url:yelpResult['mobile_url']).exists?)
+          isOpenAndPrice = GooglePlaces.isOpenAndPrice(getFormattedAddressFromYelpResult(yelpResult), @invitation.dayOfWeek, @invitation.timeOfDay)
+          restaurant = @invitation.restaurants.create({:name => yelpResult['name'], :price => isOpenAndPrice.price, :address => yelpResult['location']['display_address'] * ",", :url => yelpResult['mobile_url'], :rating_img => yelpResult['rating_img_url'], :snippet_img => yelpResult['image_url'], :rating => yelpResult['rating'], :categories => yelpResult['categories'], :review_count => yelpResult['review_count'], :open_start => isOpenAndPrice.open_start, :open_end => isOpenAndPrice.open_end, :open => isOpenAndPrice.open, :distance => yelpResult['distance']})
           categoryMutex.synchronize{viableOptions += 1} if restaurant.open 
         end
       end
