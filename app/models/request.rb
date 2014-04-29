e6817c3933a25b10f17b6f2f7705cfff586b7879
Ryan Kass <kassr30@gmail.com>
2014-04-29 10:50:10 -0400

    x

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,19 @@
 class Request < ActiveRecord::Base
+
+  def referenceNumber
+    self.url[self.url.index('=') + 1..self.url.index('&') - 1]
+  end
+
+  def query
+    self.url[self.url.index('query=') + 6..self.url.index('&sensor') -1]
+  end
+
+  def makeRequest(client)
+    if self.url.include?"detail"
+      client.spot(self.referenceNumber)
+    else
+      client.spot_by_query(self.query)
+    end
+  end
+
 end
