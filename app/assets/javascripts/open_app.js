27ec6c1f777bb2a38aa24320faa6ea0be32dd843
Ryan Kass <kassr30@gmail.com>
2014-05-22 12:54:34 -0400

    x

--- app/assets/javascripts/open_app.js
+++ app/assets/javascripts/open_app.js
@@ -1,10 +1,10 @@
 var timeout;
-function open_appstore(auth_token) {
-  window.location='letsEat://' + 'register/' + auth_token;
+function open_appstore() {
+  window.location='letsEat://'
 }
 
 function try_to_open_app(auth_token) {
-  timeout = setTimeout('open_appstore(auth_token)', 300);
+  timeout = setTimeout('open_appstore()', 300);
 }
 
 function helloWorld(){
