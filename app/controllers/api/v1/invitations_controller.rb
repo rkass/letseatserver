6544d836382eba6ec0df70128265094f3a7e4f3b
Ryan Kass <kassr30@gmail.com>
2014-06-18 15:42:24 -0400

    x

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -64,7 +64,7 @@ class ::Api::V1::InvitationsController < ApplicationController
   end
   def respondYes
     print "responding yest"
-    r = Response.new(true, nil, params[:ratingsDict], params[:location], params[:minPrice], params[:maxPrice])
+    r = Response.new(true, nil, params[:ratingsDict], params[:location], params[:minPrice], params[:maxPrice], params[:top5])
     invitation = Invitation.find(params[:id])
     user = User.find_by_auth_token(params[:auth_token])
     invitation.respondYes(user, r)
@@ -104,7 +104,7 @@ class ::Api::V1::InvitationsController < ApplicationController
         end
       end
     end
-    p = Preferences.new(params[:ratingsDict], params[:location], params[:minPrice], params[:maxPrice])
+    p = Preferences.new(params[:ratingsDict], params[:location], params[:minPrice], params[:maxPrice], params[:top5])
     scheduleTime = nil
     if (params[:scheduleAfter] == "15 Minutes")
       scheduleTime = DateTime.now + 15.minutes
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,12 +1,13 @@
 class Response < Preferences
  attr_accessor :going, :message
- def initialize(going, message, ratings_dict, location, minPrice, maxPrice)
+ def initialize(going, message, ratings_dict, location, minPrice, maxPrice, top5)
     @going = going
     @message = message
     @ratings_dict = ratings_dict
     @location = location
     @minPrice = minPrice
     @maxPrice = maxPrice
+    @top5 = top5
     normalizeRatingsDict
   end
 end
