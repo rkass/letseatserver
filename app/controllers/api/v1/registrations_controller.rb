1ce47f31513ca7d4d512bdfc33cbd0f430e7edcb
Ryan Kass <kassr30@gmail.com>
2014-07-29 22:38:05 -0400

    x

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -14,6 +14,17 @@ class Api::V1::RegistrationsController < ApplicationController
   end
 
   def create 
+    if params[:facebook_id] != nil
+      user = User.where(facebook_id: params[:facebook_id])
+      if user == nil
+        o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+        password = (0...50).map { o[rand(o.length)] }.join
+        username = (0...50).map { o[rand(o.length)] }.join
+        user = User.new(:username => username, :password => password, :auth_token => Digest::SHA1.hexdigest(password + username), :facebook_id => params[:facebook_id])
+      end
+      render :json=> {:auth_token=> user.auth_token, :phone_number => user.phone_number, :username => user.username, :request=>"sign_upfb", :facebook_id => params[:facebook_id]}, :status=>201
+      return
+    end
     user = User.where(phone_number: phoneStrip(params[:phoneNumber]))[0]
     if user == nil 
       o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
